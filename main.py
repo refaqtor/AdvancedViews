cd6be74bbe240b791d59fc7d013bbc999f3b98ce
Filippo Cucchetto <filippocucchetto@gmail.com>
2017-10-07 16:29:03 +0200

    Implemented the rect intersection

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ class Axis:
         (pos, size) = element
         return self.remove_at(pos)
 
+    @property
     def length(self):
         """
         Return the number of elements contained in this Axis
@@ -82,6 +83,7 @@ class Axis:
             result += r.num_elements
         return result
 
+    @property
     def visual_length(self):
         """
         Return the total visual length of the axis elements
@@ -181,26 +183,26 @@ class AxisTest(unittest.TestCase):
         self.assertEqual(self.axis.visual_get(250), None)
 
     def test_length(self):
-        self.assertEqual(self.axis.length(), 0)
+        self.assertEqual(self.axis.length, 0)
         self.axis.append(100)
-        self.assertEqual(self.axis.length(), 1)
+        self.assertEqual(self.axis.length, 1)
         self.axis.append(50)
-        self.assertEqual(self.axis.length(), 2)
+        self.assertEqual(self.axis.length, 2)
         self.axis.append(100)
-        self.assertEqual(self.axis.length(), 3)
+        self.assertEqual(self.axis.length, 3)
         self.axis.append(100)
-        self.assertEqual(self.axis.length(), 4)
+        self.assertEqual(self.axis.length, 4)
 
     def test_visual_length(self):
-        self.assertEqual(self.axis.visual_length(), 0)
+        self.assertEqual(self.axis.visual_length, 0)
         self.axis.append(100)
-        self.assertEqual(self.axis.visual_length(), 100)
+        self.assertEqual(self.axis.visual_length, 100)
         self.axis.append(50)
-        self.assertEqual(self.axis.visual_length(), 150)
+        self.assertEqual(self.axis.visual_length, 150)
         self.axis.append(100)
-        self.assertEqual(self.axis.visual_length(), 250)
+        self.assertEqual(self.axis.visual_length, 250)
         self.axis.append(100)
-        self.assertEqual(self.axis.visual_length(), 350)
+        self.assertEqual(self.axis.visual_length, 350)
 
     def test_remove_at(self):
         self.axis.append(100)
@@ -285,14 +287,89 @@ class Rect:
         """
         Return the intersection of this rect with other
         """
-        if self.top_left.x > other.bottom_right.x \
-            or self.bottom_right.x < other.top_left.x \
-            or self.top_left.y > other.bottom_right.y \
-            or self.bottom_right.y > other.top_left.y:
+        if self.top_left.x >= other.bottom_right.x \
+            or self.bottom_right.x <= other.top_left.x \
+            or self.top_left.y >= other.bottom_right.y \
+            or self.bottom_right.y <= other.top_left.y:
             return None
-        return None
+        top_left = Point(max(self.top_left.x, other.top_left.x),
+                         max(self.top_left.y, other.top_left.y))
+        bottom_right = Point(min(self.bottom_right.x, other.bottom_right.x),
+                             min(self.bottom_right.y, other.bottom_right.y))
+        if top_left.x >= bottom_right.x \
+           or top_left.y >= bottom_right.y:
+            return None
+        return Rect.from_points(top_left, bottom_right)
+
+    @property
+    def width(self):
+        """
+        Return the rectangle width
+        """
+        return self.right - self.left
+
+    @width.setter
+    def width(self, width):
+        self.right = self.left + width
+
+    @property
+    def height(self):
+        """
+        Return the rectangle height
+        """
+        return self.bottom - self.top
+
+    @height.setter
+    def height(self, height):
+        self.bottom = self.top + height
+
+    @property
+    def left(self):
+        """
+        Return the left edge coordinate
+        """
+        return self.top_left.x
+
+    @left.setter
+    def left(self, value):
+        self.top_left.x = value
+
+    @property
+    def right(self):
+        """
+        Return the right edge coordinate
+        """
+        return self.bottom_right.x
+
+    @right.setter
+    def right(self, value):
+        self.bottom_right.x = value    
+
+    @property
+    def top(self):
+        """
+        Return the top edge coordinate
+        """
+        return self.top_left.y
+
+    @top.setter
+    def top(self, value):
+        self.top_left.y = value
+
+    @property
+    def bottom(self):
+        """
+        Return the bottom edge coordinate
+        """
+        return self.bottom_right.y
+
+    @bottom.setter
+    def bottom(self, value):
+        self.bottom_right.y = value
 
     def __eq__(self, other):
+        if other is None:
+            return False
         return self.top_left == other.top_left and \
                self.bottom_right == other.bottom_right
 
@@ -308,6 +385,29 @@ class RectTest(unittest.TestCase):
         rect = Rect.from_xy(0, 0, 100, 100)
         self.assertEqual(rect.intersection(Rect.from_xy(100, 100, 200, 200)), None)
         self.assertEqual(rect.intersection(Rect.from_xy(50, 50, 100, 100)), Rect.from_xy(50, 50, 100, 100))
+        self.assertEqual(rect.intersection(Rect.from_xy(0, 0, 100, 100)), Rect.from_xy(0, 0, 100, 100))
+        self.assertEqual(rect.intersection(Rect.from_xy(-20, -20, 20, 20)), Rect.from_xy(0, 0, 20, 20))
+        self.assertEqual(rect.intersection(Rect.from_xy(80, 80, 120, 120)), Rect.from_xy(80, 80, 100, 100))
+        self.assertEqual(rect.intersection(Rect.from_xy(120, 120, 140, 140)), None)
+        self.assertEqual(rect.intersection(Rect.from_xy(-20, 20, 0, 0)), None)
+
+    def test_width_and_height(self):
+        rect = Rect.from_xy(0, 0, 100, 80)
+        self.assertEqual(rect.width, 100)
+        self.assertEqual(rect.height, 80)
+        rect.width = 50
+        self.assertEqual(rect.width, 50)
+        self.assertEqual(rect.right, 50)
+        rect.height = 40
+        self.assertEqual(rect.bottom, 40)
+        self.assertEqual(rect.height, 40)
+
+    def test_edges(self):
+        rect = Rect.from_xy(0, 20, 100, 100)
+        self.assertEqual(rect.left, 0)
+        self.assertEqual(rect.top, 20)
+        self.assertEqual(rect.right, 100)
+        self.assertEqual(rect.bottom, 100)
 
 
 @functools.total_ordering
@@ -338,24 +438,29 @@ class Table:
         self.xAxis = Axis()
         self.yAxis = Axis()
 
+    @property
     def bounding_rect(self):
         """
         Return the visual rect for the given table
         :return:
         """
         top_left = Point(0, 0)
-        bottom_right = Point(self.xAxis.visual_length(),
-                             self.yAxis.visual_length())
+        bottom_right = Point(self.xAxis.visual_length,
+                             self.yAxis.visual_length)
         return Rect(top_left, bottom_right)
+
+    @bounding_rect.setter
+    def bounding_rect(self, value):
+        raise Exception('Bounding rect of Table is readonly')
 
     def cells_in_visual_rect(self, rect):
         """
         Return the cells in the given visual rect
         """
         x_min = max(rect.top_left.x, 0)
-        x_max = min(rect.bottom_right.x, self.xAxis.visual_length() - 1)
+        x_max = min(rect.bottom_right.x, self.xAxis.visual_length - 1)
         y_min = max(rect.top_left.y, 0)
-        y_max = min(rect.bottom_right.y, self.yAxis.visual_length() - 1)
+        y_max = min(rect.bottom_right.y, self.yAxis.visual_length - 1)
         column_min, _ = self.xAxis.visual_get(x_min)
         column_max, _ = self.xAxis.visual_get(x_max)
         row_min, _ = self.yAxis.visual_get(y_min)
@@ -367,16 +472,15 @@ class Table:
         result.sort()
         return result
 
-
 class TableTest(unittest.TestCase):
     def setUp(self):
         self.table = Table()
 
     def test_bounding_rect(self):
-        self.assertEqual(self.table.bounding_rect(), Rect.from_xy(0, 0, 0, 0))
+        self.assertEqual(self.table.bounding_rect, Rect.from_xy(0, 0, 0, 0))
         self.table.xAxis.append(100)
         self.table.yAxis.append(50)
-        self.assertEqual(self.table.bounding_rect(), Rect.from_xy(0, 0, 100, 50))
+        self.assertEqual(self.table.bounding_rect, Rect.from_xy(0, 0, 100, 50))
 
     def test_cells_in_rect(self):
         self.table.xAxis.append(100)
@@ -384,7 +488,7 @@ class TableTest(unittest.TestCase):
         self.table.yAxis.append(50)
         self.table.yAxis.append(50)
         self.table.yAxis.append(50)
-        self.assertEqual(self.table.bounding_rect(), Rect.from_xy(0, 0, 200, 150))
+        self.assertEqual(self.table.bounding_rect, Rect.from_xy(0, 0, 200, 150))
         cells = self.table.cells_in_visual_rect(Rect.from_xy(0, 0, 200, 150))
         self.assertEqual(cells, [Cell(0, 0), Cell(0, 1),
                                  Cell(1, 0), Cell(1, 1),
